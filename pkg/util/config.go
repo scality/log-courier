07edfa954f516c93c8f65ba6ce8c8b210f5e35fd
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2025-10-27 14:06:32 +0200

    LOGC-2: Add configuration management utilities and dependencies
    Also add testing, configuration and ClickHouse dependencies.

---
+++ pkg/util/config.go
@@ -0,0 +1,85 @@
+package util
+
+import (
+	"fmt"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+// ConfigSpec represents a full configuration specification as a map
+// of configuration item names associated with their description
+type ConfigSpec map[string]ConfigVarSpec
+
+// ConfigVarSpec describes a configuration item in a ConfigSpec
+type ConfigVarSpec struct {
+	Help         string
+	DefaultValue interface{}
+	EnvVar       string
+}
+
+// LoadConfiguration loads and checks a hierarchy of configuration
+// values based on the specification. Configuration values are taken:
+//
+// - from a configuration file in YAML format (if configPath is not an empty string)
+//
+// - and/or from environment variables
+//
+// - and/or from a flag set (typically generated from command-line flags)
+//
+// configPath is the path to the configuration file, or an empty
+// string if there is no configuration file to load.
+func (configSpec *ConfigSpec) LoadConfiguration(configPath string,
+	configPrefix string, requiredPrefixes []string) error {
+	if configPath != "" {
+		viper.SetConfigType("yaml")
+		viper.SetConfigFile(configPath)
+		err := viper.ReadInConfig()
+		if err != nil {
+			return fmt.Errorf("cannot read config: %w", err)
+		}
+	}
+	for configVarName, configVarSpec := range *configSpec {
+		viper.SetDefault(configVarName, configVarSpec.DefaultValue)
+		if configVarSpec.EnvVar != "" {
+			_ = viper.BindEnv(configVarName, configVarSpec.EnvVar)
+		}
+	}
+	return nil
+}
+
+// AddFlag creates and binds a new flag in a pflags.FlagSet to the
+// running configuration. It takes precedence over environment
+// variables or configuration files.
+//
+// Example:
+//
+//	configSpec.AddFlag(pflag.CommandLine, "my-flag", "myprog.my-flag")
+func (configSpec *ConfigSpec) AddFlag(flags *pflag.FlagSet, flagName, configVarName string) {
+	configVarSpec := (*configSpec)[configVarName]
+	switch defaultValue := configVarSpec.DefaultValue.(type) {
+	case string:
+		flags.String(flagName, defaultValue, configVarSpec.Help)
+	case int:
+		flags.Int(flagName, defaultValue, configVarSpec.Help)
+	default:
+		panic(fmt.Sprintf("invalid config var type: var=%s type=%T",
+			configVarName, configVarSpec.DefaultValue))
+	}
+	_ = viper.BindPFlag(configVarName, flags.Lookup(flagName))
+}
+
+// GetString returns a single running configuration value of type string
+func (configSpec *ConfigSpec) GetString(varName string) string {
+	return viper.GetString(varName)
+}
+
+// GetInt returns a single running configuration value of type int
+func (configSpec *ConfigSpec) GetInt(varName string) int {
+	return viper.GetInt(varName)
+}
+
+// Reset resets the configuration values (only for testing)
+func (configSpec *ConfigSpec) Reset() {
+	viper.Reset()
+}
