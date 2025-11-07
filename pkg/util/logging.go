cffea9f8ccb8f2a42e92dc45bbcb42784b89518d
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2025-11-07 17:16:14 +0200

    LOGC-7: Add utility for parsing log levels

---
+++ pkg/util/logging.go
@@ -0,0 +1,20 @@
+package util
+
+import "log/slog"
+
+// ParseLogLevel converts a string log level to slog.Level.
+// Returns slog.LevelInfo for unknown levels.
+func ParseLogLevel(level string) slog.Level {
+	switch level {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
