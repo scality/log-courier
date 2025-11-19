966061db69e505cadba41549c2b0591963ae9b23
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2025-11-19 19:08:44 +0200

    LOGC-14: Add RaftSessionID to LogBatch struct
    This is needed so that we fetch logs using both bucket name
    and raft session ID (WHERE bucketName = ? AND raftSessionID = ?)

--- pkg/logcourier/logbatch.go
+++ pkg/logcourier/logbatch.go
@@ -7,10 +7,11 @@ import (
 
 // LogBatch represents a batch of logs ready for processing
 type LogBatch struct {
-	MinTimestamp time.Time
-	MaxTimestamp time.Time
-	Bucket       string
-	LogCount     uint64
+	MinTimestamp  time.Time
+	MaxTimestamp  time.Time
+	Bucket        string
+	RaftSessionID uint16
+	LogCount      uint64
 }
 
 // String returns a string representation for logging
