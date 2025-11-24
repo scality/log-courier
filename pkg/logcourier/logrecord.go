09c0a7484f0ac64cae38d6be834c92281800d3c0
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2025-11-24 21:39:40 +0200

    LOGC-17: Sync LogRecord fields with Federation ClickHouse scheme

--- pkg/logcourier/logrecord.go
+++ pkg/logcourier/logrecord.go
@@ -4,40 +4,42 @@ import "time"
 
 // LogRecord represents a single log record from ClickHouse
 // Maps to the AWS S3 Server Access Log format fields
+//
+//nolint:govet // fieldalignment: logical field grouping preferred over minor memory optimization
 type LogRecord struct {
-	StartTime  time.Time `ch:"startTime"`  // AWS field 3: Time (request start time)
-	InsertedAt time.Time `ch:"insertedAt"` // ClickHouse insertion timestamp (for offset tracking)
-	Timestamp  time.Time `ch:"timestamp"`  // Event timestamp
+	// AWS S3 Server Access Log fields (ordered by AWS format)
+	// https://docs.aws.amazon.com/AmazonS3/latest/userguide/LogFormat.html
+	BucketOwner        string    `ch:"bucketOwner"`        // 1. Bucket Owner
+	BucketName         string    `ch:"bucketName"`         // 2. Bucket
+	StartTime          time.Time `ch:"startTime"`          // 3. Time
+	ClientIP           string    `ch:"clientIP"`           // 4. Remote IP
+	Requester          string    `ch:"requester"`          // 5. Requester
+	ReqID              string    `ch:"req_id"`             // 6. Request ID
+	Operation          string    `ch:"operation"`          // 7. Operation
+	ObjectKey          string    `ch:"objectKey"`          // 8. Key
+	RequestURI         string    `ch:"requestURI"`         // 9. Request-URI
+	HttpCode           uint16    `ch:"httpCode"`           // 10. HTTP status
+	ErrorCode          string    `ch:"errorCode"`          // 11. Error Code
+	BytesSent          uint64    `ch:"bytesSent"`          // 12. Bytes Sent
+	ObjectSize         uint64    `ch:"objectSize"`         // 13. Object Size
+	TotalTime          float32   `ch:"totalTime"`          // 14. Total Time
+	TurnAroundTime     float32   `ch:"turnAroundTime"`     // 15. Turn-Around Time
+	Referer            string    `ch:"referer"`            // 16. Referer
+	UserAgent          string    `ch:"userAgent"`          // 17. User-Agent
+	VersionID          string    `ch:"versionId"`          // 18. Version Id
+	// 19. Host Id - omitted (not supported)
+	SignatureVersion   string `ch:"signatureVersion"`   // 20. Signature Version
+	CipherSuite        string `ch:"cipherSuite"`        // 21. Cipher Suite
+	AuthenticationType string `ch:"authenticationType"` // 22. Authentication Type
+	HostHeader         string `ch:"hostHeader"`         // 23. Host Header
+	TlsVersion         string `ch:"tlsVersion"`         // 24. TLS version
+	// 25. Access Point ARN - omitted (not supported)
+	AclRequired string `ch:"aclRequired"` // 26. ACL Required
 
-	BucketOwner        string `ch:"bucketOwner"`        // AWS field 1: Bucket Owner
-	BucketName         string `ch:"bucketName"`         // AWS field 2: Bucket
-	ClientIP           string `ch:"clientIP"`           // AWS field 4: Remote IP
-	Requester          string `ch:"requester"`          // AWS field 5: Requester
-	ReqID              string `ch:"req_id"`             // AWS field 6: Request ID
-	Action             string `ch:"action"`             // AWS field 7: Operation
-	ObjectKey          string `ch:"objectKey"`          // AWS field 8: Key
-	HttpURL            string `ch:"httpURL"`            // AWS field 9: Request-URI
-	ErrorCode          string `ch:"errorCode"`          // AWS field 11: Error Code
-	Referer            string `ch:"referer"`            // AWS field 16: Referer
-	UserAgent          string `ch:"userAgent"`          // AWS field 17: User-Agent
-	VersionID          string `ch:"versionId"`          // AWS field 18: Version Id
-	SignatureVersion   string `ch:"signatureVersion"`   // AWS field 20: Signature Version
-	CipherSuite        string `ch:"cipherSuite"`        // AWS field 21: Cipher Suite
-	AuthenticationType string `ch:"authenticationType"` // AWS field 22: Authentication Type
-	HostHeader         string `ch:"hostHeader"`         // AWS field 23: Host Header
-	TlsVersion         string `ch:"tlsVersion"`         // AWS field 24: TLS Version
-	AclRequired        string `ch:"aclRequired"`        // AWS field 26: ACL Required
-
-	// Internal
-	LoggingTargetBucket string `ch:"loggingTargetBucket"` // Destination bucket for log object
-	LoggingTargetPrefix string `ch:"loggingTargetPrefix"` // Prefix for log object key
-
-	BytesSent     uint64 `ch:"bytesSent"`     // AWS field 12: Bytes Sent
-	ContentLength uint64 `ch:"contentLength"` // AWS field 13: Object Size
-
-	ElapsedMs      float32 `ch:"elapsed_ms"`     // AWS field 14: Total Time
-	TurnAroundTime float32 `ch:"turnAroundTime"` // AWS field 15: Turn-Around Time
-
-	HttpCode      uint16 `ch:"httpCode"`      // AWS field 10: HTTP Status
-	RaftSessionID uint16 `ch:"raftSessionID"` // Bucket raft session ID
+	// Internal fields (not part of AWS log format)
+	Timestamp           time.Time `ch:"timestamp"`           // Event timestamp (for ordering, key generation)
+	InsertedAt          time.Time `ch:"insertedAt"`          // ClickHouse insertion timestamp (for offset tracking)
+	LoggingTargetBucket string    `ch:"loggingTargetBucket"` // Destination bucket for log object
+	LoggingTargetPrefix string    `ch:"loggingTargetPrefix"` // Prefix for log object key
+	RaftSessionID       uint16    `ch:"raftSessionID"`       // Bucket raft session ID (for ordering, offset tracking)
 }
