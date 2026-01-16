8d1eeaf48dcb6d7cfe4fe35eea7c2e364158e0ed
Mickael Bourgois <mickael.bourgois@scality.com>
2026-01-16 18:06:52 +0100

    LOGC-9: Add Discovery metrics

--- pkg/logcourier/metrics.go
+++ pkg/logcourier/metrics.go
@@ -7,7 +7,8 @@ import (
 
 // Metrics holds all Prometheus metrics for log-courier, grouped by operation
 type Metrics struct {
-	General GeneralMetrics
+	General   GeneralMetrics
+	Discovery DiscoveryMetrics
 }
 
 // GeneralMetrics tracks general system state and errors
@@ -17,6 +18,13 @@ type GeneralMetrics struct {
 	BatchProcessingDuration prometheus.Histogram
 	CyclesTotal prometheus.Counter
 	CycleDuration prometheus.Histogram
+}
+
+// DiscoveryMetrics tracks batch discovery operations
+type DiscoveryMetrics struct {
+	BatchesFound prometheus.Counter
+	BucketsPerDiscovery prometheus.Gauge
+	Duration prometheus.Histogram
 }
 
 // NewMetrics creates and registers all Prometheus metrics
@@ -30,7 +38,8 @@ func NewMetricsWithRegistry(reg prometheus.Registerer) *Metrics {
 	factory := promauto.With(reg)
 
 	return &Metrics{
-		General: newGeneralMetrics(factory),
+		General:   newGeneralMetrics(factory),
+		Discovery: newDiscoveryMetrics(factory),
 	}
 }
 
@@ -70,4 +79,28 @@ func newGeneralMetrics(factory promauto.Factory) GeneralMetrics {
 			},
 		),
 	}
+}
+
+func newDiscoveryMetrics(factory promauto.Factory) DiscoveryMetrics {
+	return DiscoveryMetrics{
+		BatchesFound: factory.NewCounter(
+			prometheus.CounterOpts{
+				Name: "log_courier_discovery_batches_found_total",
+				Help: "Total number of log batches discovered during work discovery",
+			},
+		),
+		BucketsPerDiscovery: factory.NewGauge(
+			prometheus.GaugeOpts{
+				Name: "log_courier_discovery_buckets_per_discovery",
+				Help: "Number of buckets discovered",
+			},
+		),
+		Duration: factory.NewHistogram(
+			prometheus.HistogramOpts{
+				Name:    "log_courier_discovery_duration_seconds",
+				Help:    "Time spent discovering batches in ClickHouse",
+				Buckets: []float64{0.1, 0.5, 1, 2, 5, 10, 20}, // 100ms to 20s
+			},
+		),
+	},
 }
